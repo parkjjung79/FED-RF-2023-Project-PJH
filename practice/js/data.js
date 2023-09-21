20fe3f1bfce2204ed0d3f781f8cdc65fb8227c58
parkjjung79 <parkjjung79@naver.com>
2023-09-21 14:44:38 +0900

    ss

--- practice/js/data.js
+++ practice/js/data.js
@@ -6,31 +6,31 @@
 // 1. 현재상영영화 - 배열데이터안에 객체형식
  
 const mvList = {
-    드라마:[
+    "드라마":[
       {이미지:"../images/genre/drama/mv_drama1.jpg",제목:"벌새"},
       {이미지:"../images/genre/drama/mv_drama2.jpg",제목:"플로리다프로젝트"},
       {이미지:"../images/genre/drama/mv_drama3.jpg",제목:"그린북"},
       {이미지:"../images/genre/drama/mv_drama4.jpg",제목:"미나리"},
       {이미지:"../images/genre/drama/mv_drama5.jpg",제목:"택시운전사"},],
-    로맨스:[
+    "로맨스":[
       {이미지:"../images/genre/romance/mv_romance1.jpg",제목:"몽루아"},
       {이미지:"../images/genre/romance/mv_romance2.jpg",제목:"이터널선샤인"},
       {이미지:"../images/genre/romance/mv_romance3.jpg",제목:"블루발렌타인"},
       {이미지:"../images/genre/romance/mv_romance4.jpg",제목:"비포선라이즈"},
       {이미지:"../images/genre/romance/mv_romance5.jpg",제목:"어바웃타임"},],
-    공포:[
+    "공포":[
       {이미지:"../images/genre/horror/mv_horror1.jpg",제목:"미드소마"},
       {이미지:"../images/genre/horror/mv_horror2.jpg",제목:"보이즈어프레이드"},
       {이미지:"../images/genre/horror/mv_horror3.jpg",제목:"유전"},
       {이미지:"../images/genre/horror/mv_horror4.jpg",제목:"랑종"},
       {이미지:"../images/genre/horror/mv_horror5.jpg",제목:"곡성"},],
-    판타지:[
+    "판타지":[
       {이미지:"../images/genre/fantasy/mv_fantasy1.jpg",제목:"헝거게임"},
       {이미지:"../images/genre/fantasy/mv_fantasy2.jpg",제목:"해리포터"},
       {이미지:"../images/genre/fantasy/mv_fantasy3.jpg",제목:"신비한동물들과덤블도어의비밀"},
       {이미지:"../images/genre/fantasy/mv_fantasy4.jpg",제목:"트와일라잇"},
       {이미지:"../images/genre/fantasy/mv_fantasy5.jpg",제목:"헬보이"},],
-    애니메이션:[
+    "애니메이션":[
       {이미지:"../images/genre/animation/mv_ani1.jpg",제목:"토이스토리4"},
       {이미지:"../images/genre/animation/mv_ani2.jpg",제목:"코코"},
       {이미지:"../images/genre/animation/mv_ani3.jpg",제목:"센과치히로의생방불명"},
@@ -42,5 +42,40 @@ const mvList = {
 
 
 
+    
+    
+    
+    // 스페셜관 구성 데이터
+    /******************************************** 
+      [ 데이터 항목 : ]
+      1. 순번(기본키) : idx
+      2. 스페셜관이름 : title
+      3. 스페셜관정보 : info
+      ********************************************/
+    const spList = [
+      {
+        idx: "1",
+        title: "돌비시네마",
+        story: `단지, 보고, 듣는 영화에 만족할 수 없는 당신을 위해! 드라마틱한 이미지, 입체적인 사운드, 몰입에 최적화 된 공간까지! 최상의 영화를 경험하는 단 하나의 시네마, Dolby Cinema 를 메가박스에서 만나보세요.`,
+      },
+      {
+        idx: "2",
+        title: "더 부티크 프라이빗",
+        story: `당신의 특별한 순간을 빛나게 해줄 프리미엄 어메니티와 룸서비스를 메가박스에서 만나보세요.`,
+      },
+      {
+        idx: "3",
+        title: "퍼피시네마",
+        story: `세계 최초 반려동물 동반 멀티플렉스! 강아지와 영화는 물론 미용,목욕,보딩,식사까지 모든 것을 오롯이 함께 즐길 수 있는 우리들의 공간을 메가박스에서 만나보세요.`,
+      },
+      {
+        idx: "4",
+        title: "키즈시네마",
+        story: `아이와 가족이 함께 머물며 삶의 소중한 가치를 배우는 더 행복한 놀이공간을 메가박스에서 만나보세요.`,
+      },
+      
+    ]; //////////////// spList ////////////////////////
+    
+    
     // 모듈화 내보내기 ///////
-    export {mvList};
+    export {mvList, spList};
--- practice/js/main.js
+++ practice/js/main.js
@@ -34,59 +34,18 @@ dFn.addEvt(window,'keyup',()=>setPos(window.scrollY));
 // 이벤트 객체의 이벤트 버블링 막아주는 메서드임!
 
 
-
-
-  // gr_button 링크걸기 ////////////////////////////////////////
-
-  // 1. 대상선정: .grmenu>ul>li>a 
-  // let grbtn = qsa('.grmenu>ul>li>a');
-  // console.log('장르영화',grbtn);
-
-  // // 장르 개수
-  // let genre_cnt = grbtn.length;
-
-  // // for문으로 버튼들을 click이벤트 설정함!
-  // // for(시;한;증){코드}
-  // for ( let i = 0; i < genre_cnt; i++) {
-  //   grbtn[i].onclick = function(){
-  //     let btxt = this.innerText;
-  //     console.log('난누구?',btxt);
-
-
-
-
-  //   }; ////////////// click 이벤트함수 //////////////
-
-
-  // } //////////////////// for //////////////////////////
-
-
-
-
-
-// 스페셜관 JS
-
-/* 
-      1. 요구사항 : 
-      1)스페셜관 이름을 클릭하면
-      .sp_menu > .partbox > div > span::after -> width 길이가 100%로 진행됨
-
-      2) width 길이가 100% 되면 스페셜관 화면이 전환됨
-        -> width: 100% 되는 시간과 스페셜관 배경 전환시간을 동일하게 지정!
-    */
-
-// 2. 대상선정
-
-
-
-
-
-// 메인배너 클릭시 영상 재생
-const banner = dFn.qs(".main_banner");
+// 메인배너 클릭시 영상 재생 ///////////////////////////
+const banner = dFn.qs(".banner_img");
 
 dFn.addEvt(banner,'click',()=>{
   // console.log(33);
-  banner.innerHTML = `<iframe width="80%" height="80%" src="https://www.youtube.com/embed/KI322kD7lNk?autoplay=1" title="베놈 메인 예고편" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+  banner.innerHTML = `
+  <div id="mv">
+  <iframe width="100%" height="100%" src="https://www.youtube.com/embed/KI322kD7lNk?autoplay=1" title="베놈 메인 예고편" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <!--닫기버튼-->
+  <button class="cbtn">×</button>
+  </div>
+  `;
 
 
   // 닫기버튼 클릭시 #mv 제거하기
@@ -99,6 +58,25 @@ dFn.addEvt(banner,'click',()=>{
   // 가림막보이기 body요소에 클래스 on 주기
   document.body.classList.add('on');
 }); ///////// click /////////
+
+
+
+
+
+  // 장르버튼 활성화하기 ///////////////////////////
+
+  // 1. 대상선정: .grmenu>ul>li>a 
+  let grbtn =  dFn.qsaEl('.grmenu>ul>li>a');
+  console.log(grbtn);
+
+  
+
+ 
+
+
+
+
+
 
 
 
@@ -121,3 +99,93 @@ setInterval(() => {
 
 
 
+// 메인배너 버튼으로 이미지 슬라이드 ///////////////////////////////
+
+// 1. 기능정의: 버튼클릭시 갤러리박스를 잘라서 앞/뒤로 이동함!
+// 1-1. 오른쪽 버튼 클릭시 - 맨앞div 맨뒤로 이동
+// -> 갤러리부모박스.appendChild(맨앞자식div)
+// 1-2. 왼쪽버튼 클릭시 - 맨뒤 div 맨앞으로 이동
+// -> 갤러리부모박스.insertBefore(맨뒤자식div,맨앞자식div)
+
+// 2. 대상선정 
+// 2-1. 이벤트대상 : .abtn(이동버튼들)
+const abtn = dFn.qsa('.abtn');
+// 2-2. 변경대상 : .gbx(갤러리부모박스)
+const gbx = dFn.qs('.gbx');
+
+// console.log('대상:',abtn,gbx);
+
+// 3. 이벤트 설정하기! //////////////////
+abtn.forEach(ele=>{
+    dFn.addEvt(ele,'click',goSlide);
+})
+
+// 광클금지변수(0-허용,1-금지)
+let stsClick = 0;
+// 잠금시간
+const TIME_SLIDE = 400;
+
+// 4. 함수만들기! //////////////////////
+function goSlide(){
+    // 0. 광클금지
+    if(stsClick) return; // 돌아가!
+    stsClick=1; // 잠금!
+    setTimeout(()=>stsClick=0,TIME_SLIDE);//해제! -> 이게어려움
+
+
+
+    // 1. 버튼구분하기
+    let isR = this.classList.contains('rb');
+
+    console.log('고고씽',this,isR);
+    
+    // 현재 갤러리 하위자식 div 요소 집합 새로읽기
+    // 매번 순서가 바뀌니까 매번 새로 읽어야함!
+    let newList = dFn.qsaEl(gbx,'div');
+
+    // 2. 기능 분기하기
+    // 2-1. 오른쪽 버튼 : 맨앞div 맨뒤로 이동
+    if(isR){
+        //갤러리부모박스.appendChild(맨앞자식div)
+        gbx.appendChild(newList[0]);
+
+
+    } ///// if ////
+    // 2-2. 왼쪽 버튼 : 맨뒤div 맨 앞으로 이동
+    else{
+        // 갤러리부모박스.insertBefore(맨뒤자식div,맨앞자식div)
+        // 마지막 컬렉션 번호는 [개수-1]
+        gbx.insertBefore(newList[newList.length-1],newList[0])
+
+    } ///// else ////
+
+} //////////// goSlide 함수 ////////////
+
+
+// 자동넘김용 호출함수 //////////////////
+const goRight = () =>  gbx.appendChild(dFn.qsaEl(gbx,'div')[0]);
+
+// 자동넘김용 변수(인터발용:autoI,타임아웃용:autoT)
+let autoI, autoT;
+
+// 인터발호출함수 ///////
+const autoSlide = () => autoI = setInterval(goRight,3000);
+
+// 인터발함수 최초호출
+autoSlide();
+
+// 인터발지우기함수 //////////////
+const clearAuto = () => {
+    // 인터발 지우기
+    clearInterval(autoI);
+
+    // 타임아웃 지우기
+    clearTimeout(autoT);
+    
+    // 일정시간후 작동
+    autoT = setTimeout(autoSlide,5000);
+};
+
+
+// 버튼 클릭시 clearAuto함수 호출하기
+abtn.forEach(ele=>dFn.addEvt(ele,'click',clearAuto));
