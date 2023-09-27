da2b30dc0d71cdcc4f8234a14aef1f8f0debde84
parkjjung79 <parkjjung79@naver.com>
2023-09-27 17:50:54 +0900

    ss

---
+++ 1차프로젝트/js/fllowing.js
@@ -0,0 +1,129 @@
+// 따라다니는 원 JS - following.js
+
+// 1. 이벤트 등록 하기
+// window.addEventListener("DOMContentLoaded", setFollow);
+
+// 2. 함수만들기 -> 할당함수 /////
+// DOM선택함수
+const qs = (x) => document.querySelector(x);
+const qsa = (x) => document.querySelectorAll(x);
+
+// 2-1. 로드함수 ////
+function setFollow() {
+  // 호출확인
+  // console.log("로딩완료!");
+
+  // [1] html 코드 넣기 //////////////////////////////////
+  // 1. 대상선정
+  // 변경대상: #banner
+  const banner_box = qs("#banner");
+  // console.log("대상:", banner_box);
+
+  // 2. html 태그 만들기
+  let hcode = ""; // hcode 부분 이름 변경 가능!! // = ''; 스트링 리터럴이 됨!
+
+  // 3. 대상에 html 넣기
+  banner_box.innerHTML += hcode;
+
+  // [2] 따라다니는 원 셋팅하기
+  // 1. 대상선정 : .mover
+  // (1) 움직일 대상 : .mover
+  const mover = qs(".mover");
+  // (2) 이벤트 대상 : document.body
+  const myBody = document.body;
+
+  // console.log("요소:", mover);
+
+  /******************************************
+    
+    ★[[ 이벤트발생시 위치값 ]]★
+    1. clientX, clientY
+        -> 현재 보이는 브라우저 화면이 기준
+        -> 화면을 기준한 fixed 포지션에서 주로 사용
+
+    2. offsetX, offsetY
+        -> 이벤트 대상이 기준
+        -> 특정박스의 부모자격박스로부터 위치를 사용할 경우
+
+    3. pageX, pageY
+        -> 전체 문서를 기준(스크롤 화면을 포함)
+        -> 화면을 기준한 absoulte 포지션에서 주로 사용!
+
+
+    4. screenX, screenY
+        -> 모니터 화면을 기준
+
+    ******************************************/
+  // 무버 크기의 절반계산
+  let gap = mover.clientHeight / 2;
+  // 선택요소의 크기 js
+  // width는 clientWidth, height는 clientHeight
+  // console.log("무버Height:", gap);
+
+  // 2. 이벤트 대상에 마우스 무드 이벤트가 적용될때
+  // 무버가 따라다니게 기능구현!
+  banner_box.onmousemove = (e) => {
+    // e - 이벤트 전달변수
+    // 1. 위치값 가져오기(박스중앙위치로 보정)
+    let posx = e.pageX - gap;
+    let posy = e.pageY - gap;
+    // let posy = e.clientY - gap;
+    // -> 만약 .mover가 fixed 포지션이면
+    // 브라우저 화면에서의 위치인 clientY를 사용한다!
+
+    // 2. 무버에 위치값 적용하기
+    mover.style.top = posy + "px";
+    mover.style.left = posx + "px";
+  };
+  
+  const abtn = qsa('.abtn');
+    // 이벤트 구역을 들어올때만 보이기 / 나가면 숨기기
+    banner_box.onmouseover = () => {
+      mover.style.opacity = 1;
+      // console.log(33);
+      banner_box.style.cursor = 'none';
+
+
+      console.log(abtn);
+      abtn.forEach(ele=>{
+        ele.onmouseover = ()=>{
+          mover.style.cursor = 'pointer';
+          mover.style.transform = 'scale(0)';
+
+        }
+        ele.onmouseout = ()=>{
+          mover.style.cursor = 'none';
+          mover.style.transform = 'scale(1)';
+
+        }
+
+      })
+
+    }; ////////////// mouseenter //////////////////
+    banner_box.onmouseout = () => {
+      mover.style.opacity = 0;
+      // console.log(44);
+      
+      banner_box.style.cursor = 'auto';
+    }; ////////////// mouseleave //////////////////
+
+  // [3] a요소에 오버시 원 크게 만들기! //////////
+  // 대상: #banner
+  const mbanner = qs("#banner");
+  // console.log("메인배너:", mbanner);
+
+  // 한번에 셋팅하기!
+    // a 요소에 마우스 들어올때
+    mbanner.onmouseenter = () => (mover.style.transform = "scale(1)");
+
+    // a 요소에 마우스 나갈때
+    mbanner.onmouseleave = () => (mover.style.transform = "scale(0)");
+} //////////// loadFn /////////////////
+
+
+
+
+
+
+// 모듈 내보내기
+export default setFollow;
---
+++ 1차프로젝트/js/main.js
@@ -0,0 +1,406 @@
+// 개인 PJ main js - main.js
+
+// 모듈불러오기 ///////////////
+// DOM함수 모듈
+import dFn from "./dom.js";
+// 부드러운 스크롤 모듈
+import { startSS, setPos } from "./smoothScroll23.js";
+// 따라다니는 원 모듈
+import setFollow from "./fllowing.js";
+// 데이터 가져오기
+import {mvList, spList} from "./data.js";
+
+// console.log(dFn);
+
+// 따라다니는 원 호출
+setFollow();
+
+// 부드러운 스크롤 호출
+startSS();
+
+// 0. 새로고치면 스크롤바 위치 캐싱후 맨 위로 이동!
+setTimeout(() => {
+  // 윈도우 스크롤 맨위로!
+  window.scrollTo(0, 0);
+  // 부드러운 스크롤 위치값 반영!
+  setPos(0);
+  // 안하면 원래 위치로 스크롤 튐!
+}, 400);
+// 0. 스크롤바 트랙을 잡고 위치이동시 위치값 반영
+dFn.addEvt(window, "mouseup", () => {
+  setPos(window.scrollY);
+}); //////// mouseup ///////////
+
+// 0. 키보드 방향키 이동시 위치값 반영
+dFn.addEvt(window, "keyup", () => setPos(window.scrollY));
+//////// keyup ///////////
+
+// 부드러운 스크롤 때문에 마우스휠 이벤트 막기가 작동되어
+// 캐릭터 설명 박스 작은 스크롤도 작동안됨
+// 따라서 이벤트 버블링을 막아줘야함!
+// event.stopPropagation()
+// 이벤트 객체의 이벤트 버블링 막아주는 메서드임!
+
+const mvId = ["NHA69lCd1ZM","KI322kD7lNk","A33AdB4u8GQ","tnyWkyDGWuM"];
+
+// 메인배너 클릭시 영상 재생 ///////////////////////////
+const banner = dFn.qsa(".banner_img");
+banner.forEach((ele,idx)=>{
+  dFn.addEvt(ele, "click", () => {
+  // console.log(33);
+  playMovie(mvId[idx]);
+  }); ///////// click /////////
+
+})
+
+
+// 스페셜관 라인/배경 움직임 /////////////////////////////////////
+const spMenu = dFn.qsa(".sp_menu .partbox");
+const spTxt = dFn.qsa('.sp-txt');
+const spImg = dFn.qsa('.sp_cinema');
+console.log('나야나!',spImg);
+
+let spNum = 0;
+setInterval(() => {
+  spMenu[spNum].classList.remove("on");
+
+  spNum++;
+  if (spNum > 3) spNum = 0;
+
+  spMenu[spNum].classList.add("on");
+
+  // 아래 내용도 여기서 바꿔라명령!
+  spTxt[0].innerHTML = spList[spNum].title;
+  spTxt[1].innerHTML = spList[spNum].story;
+
+  
+}, 5000);
+
+
+
+//////////////////////////////////////
+
+
+// 전역변수구역 //////////
+// 1. 광클금지상태변수 : 0-허용,1-불허용
+let clickSts = 0;
+// 2. 슬라이드 이동시간 : 상수로 설정
+const TIME_SLIDE = 400;
+
+let CNT_SLIDE = dFn.qsa('.banner_img').length;
+const indicBox = dFn.qs('.indic');
+
+// 블릿셋팅하기! //////
+for(let i=0;i<CNT_SLIDE;i++){
+  indicBox.innerHTML+=`
+    <li${i==0?` class="on"`:``}>
+      <img src="./images/dot3.png" alt="흰색">
+      <img src="./images/dot4.png" alt="회색">
+    </li>
+  `;
+} /////////// for ///////////////
+
+// 1. 대상선정
+    // 1-1.이벤트 대상: .abtn
+    const abtn = dFn.qsa('.abtn');
+    // 1-2.변경 대상: #slide
+    const slide = dFn.qs('.banner_box');
+    // 1-3.블릿박스 대상:
+    const indic = dFn.qsa('.indic li');
+
+
+    // 대상확인
+    console.log('대상',abtn,slide,indic);
+
+    // 1.5. li리스트에 순번속성 만들어 넣기
+    // 만드는이유: 블릿변경 등에 현재 슬라이드 순번 필요!
+    // 사용자 정의 속성은 반드시 'data-'로시작해야함!(W3C규칙)
+    // data-seq 로 순번 속성을 넣을 것임!
+    dFn.qsaEl(slide,'.banner_img')
+    .forEach((ele,idx)=>ele.setAttribute('data-seq',idx));
+    // setAttribute(속성명,속성값) -> 속성셋팅 JS내장메서드
+
+
+    // 2. 이벤트 설정하기 : 버튼요소들 -> forEach()
+    abtn.forEach(ele=>dFn.addEvt(ele,'click',goSlide));
+
+    // 3. 함수만들기
+    function goSlide(){
+        // 광클금지 //////////////
+        if(clickSts) return;//나가!
+        clickSts=1;//잠금!
+        setTimeout(()=>clickSts=0,TIME_SLIDE);//해제!       
+
+
+        // 호출확인
+        console.log('나야나!',this,
+        this.classList.contains('rb'));
+
+        // classList.contains(클래스명)
+        // 선택요소에 해당클래스가 있으면 true
+
+        // 1. 오른쪽 버튼 여부 알아내기
+        let isRight = this.classList.contains('rb');
+
+        // 2. 슬라이드 li 새로 읽기
+        let eachOne = dFn.qsaEl(slide,'.banner_img');
+
+        // 3. 버튼분기하기 '.ab2' 이면 오른쪽버튼
+        if(isRight){ // 오른쪽버튼
+            // 오른쪽에서 들어오는 슬라이드함수 호출!
+            rightSlide();
+        } ////// if //////////////
+        else{ // 왼쪽버튼
+            // 1. 맨뒤li 맨앞으로 이동
+            // 놈.놈.놈 -> insertBefore(넣을놈,넣을놈전놈)
+            slide.insertBefore(
+                eachOne[eachOne.length-1], eachOne[0]);
+            // 2. left값 -100% 만들기 : 들어올 준비 위치!
+            slide.style.left = '-100%';
+            // 3. 트랜지션 없애기
+            slide.style.transition = 'none';
+            
+            // 같은 left값을 동시에 변경하면 효과가 없음!
+            // 비동기적으로 처리해야함!
+            // -> setTimeout으로 싸주기!
+            // 시간은 0이어도 비동기 처리므로 효과있음!
+
+            setTimeout(() => {
+                // 4. left값 0으로 들어오기
+                slide.style.left = '0';
+                
+                // 5. 트랜지션주기
+                slide.style.transition = 
+                    TIME_SLIDE+'ms ease-in-out';
+ 
+            }, 0);
+
+
+        } /////// else //////////////
+
+        // 4. 블릿순번 변경 함수 호출
+        chgIndic(isRight); // 방향값을 보냄!
+
+        // 5. 자동넘김 멈춤함수 호출하기
+        clearAuto();
+        
+    } ////////// goSlide 함수 /////////
+
+    // 블릿순번 변경 함수 /////////////
+    function chgIndic(isRight){ // isRight(0-왼쪽,1-오른쪽)
+        // 1. 슬라이드 순번과 일치하는 블릿에 클래스 넣기
+        // 대상: .indic li -> indic변수
+        // 맨앞 슬라이드 li의 'data-seq' 값 읽어오기
+        // isRight값이 true이면 오른쪽버튼이고 순번은 [1]
+        // isRight값이 false이면 왼쪽버튼이고 순번은 [0]
+        let nowSeq = 
+        dFn.qsaEl(slide,'.banner_img')[isRight?1:0]
+        .getAttribute('data-seq');
+
+        console.log('현재슬라이드 순번:',nowSeq);
+        
+        // 2. 해당순번 블릿li에 클래스 on넣기
+        // 블릿전체순회시 해당순번에 on넣고 나머지는 on빼기
+        indic.forEach((ele,idx)=>{
+            if(idx==nowSeq) ele.classList.add('on');
+            else ele.classList.remove('on');
+        }); ///////// forEach ///////////
+
+    } /////////// chgIndic함수 ////////////
+
+    // 슬라이드 오른쪽방향 함수 ////////////
+    function rightSlide(){
+        //1.대상이동하기
+        slide.style.left = '-100%';
+        //2.트랜지션주기
+        slide.style.transition = 
+            TIME_SLIDE+'ms ease-in-out';
+        // 이동시간 후 맨앞li 잘라서 맨뒤로 이동하기
+        // appendChild(요소)
+        setTimeout(() => {
+            // 3.맨앞li 맨뒤로 이동
+            slide.appendChild(
+              dFn.qsaEl(slide,'.banner_img')[0]);
+            // 4.slide left값 0
+            slide.style.left = '0';
+            // 5.트랜지션 없애기
+            slide.style.transition = 'none';
+        }, TIME_SLIDE);
+    } //////////// rightSlide 함수 ////////////
+
+
+    /********************************** 
+        자동넘기기 기능구현
+        -> 일정시간간격으로 오른쪽버튼 클릭
+        -> 사용JS내장함수 : setInterval()
+        -> 버튼클릭 실행 메서드: click()
+        대상: 오른쪽버튼 - .ab2 -> abtn[1]
+    **********************************/
+   // 인터발변수
+   let autoI;
+   // 인터발타이밍함수를 변수에 할당후
+   // clearInterval(할당변수) 해야 멈출 수 있다!
+
+   // 타임아웃변수
+   let autoT;
+   // 타임아웃함수도 마찬가지임!
+   // clearTimeout(할당변수) 해야 실행 쓰나미를 막을 수 있다!
+   
+   // 인터발호출 함수 //////////
+   function slideAuto(){
+       autoI= setInterval(() => {
+        // 오른쪽이동 슬라이드 함수호출
+        rightSlide();
+        // 블릿변경함수호출(오른쪽은 1)
+        chgIndic(1);
+
+       // console.log('실행!');
+       // 오른쪽버튼 클릭이벤트 강제발생!
+       // 선택요소.click()
+        //  abtn[1].click();   
+      }, 3000);
+
+   } ///////// slideAuto 함수 //////////////
+
+   // 인터발함수 최초호출!
+   slideAuto();
+
+   // 버튼을 클릭할 경우를 구분하여 자동넘김을 멈춰준다!
+   function clearAuto(){
+    // 자동넘김 지우기
+        // clearInterval(인터발할당변수)
+        console.log('멈춤!!!');
+
+        // 1. 인터발 지우기
+        clearInterval(autoI);
+        // 2. 타임아웃 지우기(재실행호출 쓰나미 방지)
+        clearTimeout(autoT);
+        // 3. 일정시간후 다시 인터발호출셋팅하기!!!
+        autoT = setTimeout(slideAuto,5000);
+        // 결과적으로 5초후 인터발재실행은 하나만 남는다!
+
+   } //////////// clearAuto 함수 ///////////
+
+
+
+
+
+/*************************************** 
+  함수명 : playMovie
+  기능: 영화예고편 화면 띄우기
+***************************************/
+function playMovie(mcode) {
+  // mcode - 영화아이디
+  // 함수호출 및 전달값 확인
+  console.log("난진짜야~!", mcode);
+
+  // 1. 동영상 박스 대상선정 : #mvbox
+  let mvbox = document.querySelector("#mvbox");
+
+  // 2. 영화박스에 아이프레임 넣기
+  mvbox.innerHTML = `
+      <div id="mv">
+          <!--유튜브 아이프레임-->
+          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${mcode}?autoplay=1" allow="autoplay"></iframe>
+          <!--닫기버튼-->
+          <button class="cbtn">×</button>
+      </div>
+  `;
+
+  // 3. 삽입된 동영상 박스 CSS설정하기
+  let mv = document.querySelector("#mv");
+  let css = mv.style;
+
+  css.position = "fixed";
+  css.top = "0";
+  css.left = "0";
+  css.width = "100%";
+  css.height = "100%";
+  css.backgroundColor = "#000";
+  css.zIndex = "99999999";
+
+  // 4. 닫기버튼 CSS 셋팅하기
+  let cbtn = document.querySelector(".cbtn");
+  // console.log(cbtn);
+  // style.cssText는
+  // css를 직접 style속성값으로 넣음!
+  cbtn.style.cssText = `
+    position: absolute;
+    top: 10%;
+    right: 10%;
+    width: 80px;
+    height: 80px;
+    border: none;
+    color: rgb(255, 255, 255);
+    border: 2px solid #fff;
+    font-size: 59px;
+    font-weight: 100;
+    border-radius: 50%;
+    cursor: pointer;
+    line-height: 29px;
+    text-indent: -3px;
+  `;
+
+  // 아이프레임 보더 없앰
+  mv.querySelector("iframe").style.border = "none";
+
+  // 5. 닫기버튼 클릭시 #mv 제거하기
+  cbtn.onclick = function () {
+      mv.remove();
+      // remove() 는 DOM 메서드로
+      // 선택요소를 제거함!
+
+      // body에 클래스 on 제거하기
+      document.body.classList.remove("on");
+  }; /////// click이벤트 함수 ///////
+
+  // 6. body요소에 클래스 on 주기
+  // 동영상 배경 암전효과
+  document.body.classList.add("on");
+} //////////////// playMovie 함수 ////////////
+//////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////
+
+// 장르버튼 활성화하기 ///////////////////////////
+
+// 1. 대상선정: .grmenu>ul>li>a
+const grmenu = dFn.qsa('.grmenu a');
+const nowmv = dFn.qsa('.nowmv li');
+// console.log(grmenu,nowmv);
+
+// 2. 이벤트 설정하기
+grmenu.forEach((ele)=>{
+  ele.onclick = ()=>{
+    event.preventDefault();
+    // 1. a텍스트 읽기
+    let atxt = ele.innerText;
+    console.log(atxt);
+    // 2. 이미지변경하기
+    nowmv.forEach((el,idx)=>{
+      // console.log(dFn.qsEl(el,'img'),mvList[atxt][idx].이미지)
+      dFn.qsEl(el,'img').src = mvList[atxt][idx].이미지;
+      dFn.qsEl(el,'img').alt = mvList[atxt][idx].제목;
+      dFn.qsEl(el,'.mv-tit').innerText = mvList[atxt][idx].제목;
+    })
+  }; ////// click /////
+}); //////// forEach /////////
+
+
+
+// 장르메뉴 클릭시 css 변경! ///////
+// 1. 대상선정 
+const nmv_click = dFn.qsa('.nowmv li');
+// 2. 이벤트 설정 : on
+
+
+
+// 3. css  style 설정
+nmv_click.style.cssText = `
+background-color: #5abec8;
+border: 1.5px solid #5abec8;
+`;
+
+
