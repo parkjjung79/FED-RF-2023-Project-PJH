de2f80b7c818a253fe74d315583eaa1443249b8c
parkjjung79 <parkjjung79@naver.com>
2023-10-17 18:17:01 +0900

    ss

---
+++ 2차프로젝트/02.구현소스/js/data_review.js
@@ -0,0 +1,22 @@
+// 아로마티카 리뷰 데이터 JS - data_review.js
+// 리뷰 총 21개 구성
+
+// 1.리뷰 - 배열데이터안에 객체형식
+ /****************************************
+ 데이터항목 :
+ 1. 순번(기본키) : idx(index)
+ 2. 제목 : title
+ 3. 이미지명 :imgName
+ 4. 날짜 : date
+ ****************************************/
+
+ const review = [
+    {idx:'1',title:'다 드립니다 ♡',imgName:'live_photo7',date:'2023-09-08'},
+    {idx:'2',title:'비하인드 짤털이~',imgName:'live_photo6',date:'2023-09-08'},
+    {idx:'3',title:'꿀 떨어지는 도깨비 내외',imgName:'live_photo5',date:'2023-09-08'},
+    {idx:'4',title:'사랑이 넘치네요',imgName:'live_photo8',date:'2023-09-08'},
+    {idx:'5',title:'덕화에게서 낯선 향기가 난다',imgName:'live_photo4',date:'2023-09-08'},
+    {idx:'6',title:'안녕하세요',imgName:'live_photo3',date:'2023-09-08'},
+    {idx:'7',title:'시청자여러분',imgName:'live_photo2',date:'2023-09-08'},
+    {idx:'8',title:'도깨비를마니마니',imgName:'live_photo1',date:'2023-09-08'},
+]; /////////////// liveDate 배열 ////////////////
