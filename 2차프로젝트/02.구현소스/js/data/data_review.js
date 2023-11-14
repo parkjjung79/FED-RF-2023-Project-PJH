1ae750110385d16149171ea215371ac8531ddc9d
parkjjung79 <parkjjung79@naver.com>
2023-11-14 13:20:47 +0900

    ss

--- 2차프로젝트/02.구현소스/js/data/data_review.js
+++ 2차프로젝트/02.구현소스/js/data/data_review.js
@@ -11,12 +11,12 @@
  ****************************************/
 
  const review = [
-    {idx:'1',title:'다 드립니다 ♡',imgName:'live_photo7',date:'2023-09-08'},
-    {idx:'2',title:'비하인드 짤털이~',imgName:'live_photo6',date:'2023-09-08'},
-    {idx:'3',title:'꿀 떨어지는 도깨비 내외',imgName:'live_photo5',date:'2023-09-08'},
-    {idx:'4',title:'사랑이 넘치네요',imgName:'live_photo8',date:'2023-09-08'},
-    {idx:'5',title:'덕화에게서 낯선 향기가 난다',imgName:'live_photo4',date:'2023-09-08'},
-    {idx:'6',title:'안녕하세요',imgName:'live_photo3',date:'2023-09-08'},
-    {idx:'7',title:'시청자여러분',imgName:'live_photo2',date:'2023-09-08'},
-    {idx:'8',title:'도깨비를마니마니',imgName:'live_photo1',date:'2023-09-08'},
+    {idx:'1',title:'다 드립니다 ♡',imgName:'review01',date:'2023-09-08'},
+    {idx:'2',title:'비하인드 짤털이~',imgName:'review01',date:'2023-09-08'},
+    {idx:'3',title:'꿀 떨어지는 도깨비 내외',imgName:'review05',date:'2023-09-08'},
+    {idx:'4',title:'사랑이 넘치네요',imgName:'review06',date:'2023-09-08'},
+    {idx:'5',title:'덕화에게서 낯선 향기가 난다',imgName:'review07',date:'2023-09-08'},
+    {idx:'6',title:'안녕하세요',imgName:'review08',date:'2023-09-08'},
+    {idx:'7',title:'시청자여러분',imgName:'review09',date:'2023-09-08'},
+    {idx:'8',title:'도깨비를마니마니',imgName:'review012',date:'2023-09-08'},
 ]; /////////////// liveDate 배열 ////////////////
