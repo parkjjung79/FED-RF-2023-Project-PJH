cce52fa5c65436b3c22123a42c587963acdd9de8
parkjjung79 <parkjjung79@naver.com>
2023-12-06 17:50:39 +0900

    ss

--- jh_PJ2/jh-pj2-app/src/content/modules/Part5.jsx
+++ jh_PJ2/jh-pj2-app/src/content/modules/Part5.jsx
@@ -4,8 +4,6 @@
 import { useEffect } from "react";
 import "../css/part5.css";
 
-// 스와이퍼 사용하기
-import { SwiperApp } from "../plugin/SwiperApp";
 
 import $ from "jquery";
 import "jquery-ui-dist/jquery-ui";
@@ -15,38 +13,10 @@ export function Part5() {
   return(
     <>
     <h1>YOU MAY ALSO LIKE</h1>
-    {/* Swiper */}
-      <div classNameName="swiper mySwiper">
-        <div className="swiper-wrapper">
-          <div className="swiper-slide"><img src="./images/product1.jpg" alt="제품1" /></div>
-          <div classNameName="name">제품명</div>
-          <div classNameName="price">가격</div>
-        </div>
-
-        <div className="swiper-wrapper">
-          <div className="swiper-slide"><img src="./images/product1.jpg" alt="제품1" /></div>
-          <div classNameName="name">제품명</div>
-          <div classNameName="price">가격</div>
-        </div>
-
-        <div className="swiper-wrapper">
-          <div className="swiper-slide"><img src="./images/product1.jpg" alt="제품1" /></div>
-          <div classNameName="name">제품명</div>
-          <div classNameName="price">가격</div>
-        </div>
-          
-        </div>
-        {/* 양쪽 이동버튼 */}
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
-        {/* 블릿 인디케이터 */}
-        <div className="swiper-pagination"></div>
-
-
-    {/* <div classNameName="porduct">
+    <div classNameName="porduct">
       <div classNameName="thumbnail">
         <img src="./images/product1.jpg" alt="제품1" />
-      </div>
+      </div>`
       <div classNameName="name">
         제품명
       </div>
@@ -73,7 +43,7 @@ export function Part5() {
       <div classNameName="price">
         가격
       </div>
-    </div> */}
+    </div>
     </>
   )
 } /////////// Part5 컴포넌트 ///////////
