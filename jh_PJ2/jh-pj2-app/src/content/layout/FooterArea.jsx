1d3e30e5b26c1ed10b6915f51465c4e37b327d7b
parkjjung79 <parkjjung79@naver.com>
2023-12-08 13:20:34 +0900

    ss

--- jh_PJ2/jh-pj2-app/src/content/layout/FooterArea.jsx
+++ jh_PJ2/jh-pj2-app/src/content/layout/FooterArea.jsx
@@ -6,16 +6,17 @@ export function FooterArea() {
       {/* <!-- 3. 하단영역 --> */}
       <div id="footer-area">
         <footer className="footer-area">
-          <div className="flogo">
-            <img src="./images/logo_w.png" alt="하단로고" />
+          <div className="csnum">
+            고객센터<br />
+            <p>
+              1600-3689
+            </p>
           </div>
-          {/* <!-- 3-2.회사주소 --> */}
-          <address className="addr">
-            서울 강남구 강남대로162길 41-4 대표. Kim Young Kyun 사업자등록번호.
-            214-86-75782 통신판매업신고. 2018-서울강남-00761 개인정보책임관리자.
-            Jang Jae Hyung 팩스. 02-540-5297 이메일. sh.lee@aromatica.co.kr ©
-            2023 AROMATICA All rights reserved. In God we trust.
-          </address>
+          <div className="hours">
+            <p>
+              월-금 / 10-17시(점심시간 13-14)
+            </p>
+          </div>
           {/* <!-- 3-3.하단링크 --> */}
           <ul className="blink">
             <li>
@@ -34,6 +35,15 @@ export function FooterArea() {
               <a href="#">인재채용</a>
             </li>
           </ul>
+          <hr />
+          {/* <!-- 3-2.회사주소 --> */}
+          <address className="addr">
+            서울 강남구 강남대로162길 41-4<br />
+            대표.Kim Young Kyun 사업자등록번호. 214-86-75782<br />
+            통신판매업신고. 2018-서울강남-00761 개인정보책임관리자.
+            Jang Jae Hyung 팩스. 02-540-5297 이메일. sh.lee@aromatica.co.kr ©
+            2023 AROMATICA All rights reserved. In God we trust.
+          </address>
         </footer>
       </div>
     </>
